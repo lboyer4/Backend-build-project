d2007e564b8d4df58caea6369c3f42ff1a1b78ec
Lauren Boyer <lboyer4@outlook.com>
2019-06-27 11:36:37 -0600

    initial set up of server.js

--- knexfile.js
+++ knexfile.js
@@ -4,15 +4,13 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      filename: 'postgres://localhost/senators',
-      useNullAsDefault: true,
-      migrations: {
-        directory: './migrations'
-      },
-      seeds: {
-        director: './seeds/dev'
-      }
+    connection:'postgres://localhost/senators',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      director: './seeds/dev'
     }
   }
 };
--- seeds/senators.js
+++ seeds/senators.js
@@ -3,45 +3,38 @@ const states = require('../states.js')
 
 exports.seed = function(knex) {
   return knex('senators').del()
+
   .then(() => {
-    return knex('states').del();
+    console.log('something')
+    knex('states').del();
   })
-  .then(() => {
-    return knex('states').insert(states)
-  })
-  .then(() => {
-      return Promise.all(
-        senators.map(senator => {
-          let {first_name, last_name, party, state_name} = senator;
-          return knex('states').where('name', state_name).select('id').first()
-          .then(stateRecord => {
-            return knex('senators').insert({first_name, last_name, party, state_id: stateRecord.id})
-          })
-        })
-      )
-    })
   // .then(() => {
-  //   let senatorPromises = [];
-  //   senators.forEach((senator) => {
-  //     let state = senator.state_name;
-  //     senatorPromises.push(createSenator(knex, senator, state));
-  //   });
-
-  //   return Promise.all(senatorPromises);
-  // });
+  //   return knex('states').insert(states)
+  // })
+  // .then(() => {
+  //     return Promise.all(
+  //       senators.map(senator => {
+  //         let {first_name, last_name, party, state_name} = senator;
+  //         return knex('states').where('name', state_name).select('id').first()
+  //         .then(stateRecord => {
+  //           return knex('senators').insert({first_name, last_name, party, state_id: stateRecord.id})
+  //         })
+  //       })
+  //     )
+  //   })
 };
 
-const createSenator = (knex, senator, state) => {
-  return knex('states').where('name', state).first()
-  .then((stateRecord) => {
-    return knex('senators').insert({
-      first_name: senator.first_name,
-      last_name: senator.last_name,
-      party: senator.party,
-      state_id: stateRecord.id
-    });
-  });
-};
+// const createSenator = (knex, senator, state) => {
+//   return knex('states').where('name', state).first()
+//   .then((stateRecord) => {
+//     return knex('senators').insert({
+//       first_name: senator.first_name,
+//       last_name: senator.last_name,
+//       party: senator.party,
+//       state_id: stateRecord.id
+//     });
+//   });
+// };
 
 
 
