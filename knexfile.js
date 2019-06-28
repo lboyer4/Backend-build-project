7763b56cb4935bd826f22c12cd6b3437f5213986
Lauren Boyer <lboyer4@outlook.com>
2019-06-28 09:37:41 -0600

    knexfile changes

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,13 @@ module.exports = {
     seeds: {
       directory: './seeds'
     }
+  },
+  production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL + `?ssl=true`,
+  migrations: {
+    directory: './db/migrations'
+  },
+  useNullAsDefault: true
   }
 };
